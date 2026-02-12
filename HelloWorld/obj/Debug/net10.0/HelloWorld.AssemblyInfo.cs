81332bcf6fd7d0d65b8bcfb25d5ab095f9e442dc
Pranitaghodke <pranitaghodke90@gmail.com>
2026-02-12 14:45:39 +0530

    added assignment

--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f248cf36182540bc90652224291fe3f8b10dd906")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf16f1ff3de982327acea0b9cc29ecbfe251de3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
